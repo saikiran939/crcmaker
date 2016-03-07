71632ae6eba8fcbc93dfa9daafe3b4cda55c38e0
Eugene Cheung <echeung@infusion.com>
2016-03-07 09:32:10 -0500

    Minor formatting/syntax changes in Gulpfile

--- README.md
+++ README.md
@@ -6,20 +6,15 @@ A simple CRC card generator. Check it out live [here](http://echeung.me/crcmaker
 ## Development
 
 ### Prerequisites
-- [Node.js and npm](https://nodejs.org/)
+- [Node.js v4+](https://nodejs.org/)
 
 ### Dependencies
 
 To install the required dependencies, simply run the following commands in the root directory of the project.
 
 ```shell
-$ npm install
-```
-
-You'll also need to install [Gulp](http://gulpjs.com/) globally:
-
-```shell
 $ npm install -g gulp
+$ npm install
 ```
 
 ### Development tasks
@@ -30,10 +25,10 @@ To compile the assets, run:
 $ gulp
 ```
 
-If you want to automatically recompile on file changes, run:
+If you want to launch a development server and automatically recompile on file changes, run:
 
 ```shell
 $ gulp watch
 ```
 
-The built files will be located in `crcmaker/build/`.
+This will automatically open up <http://localhost:3000/> in your default browser.
--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,99 @@
 'use strict';
 
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 // Dependencies                                                               //
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 
-var babelify    = require('babelify'),
-    browserify  = require('browserify'),
-    browserSync = require('browser-sync').create(),
-    buffer      = require('vinyl-buffer'),
-    del         = require('del'),
-    ghPages     = require('gulp-gh-pages'),
-    gulp        = require('gulp'),
-    htmlmin     = require('gulp-htmlmin'),
-    nano        = require('gulp-cssnano'),
-    sass        = require('gulp-sass'),
-    source      = require('vinyl-source-stream'),
-    uglify      = require('gulp-uglify');
+const babelify  = require('babelify'),
+  browserify  = require('browserify'),
+  browserSync = require('browser-sync').create(),
+  buffer      = require('vinyl-buffer'),
+  del         = require('del'),
+  ghPages     = require('gulp-gh-pages'),
+  gulp        = require('gulp'),
+  htmlmin     = require('gulp-htmlmin'),
+  nano        = require('gulp-cssnano'),
+  sass        = require('gulp-sass'),
+  source      = require('vinyl-source-stream'),
+  uglify      = require('gulp-uglify');
 
 
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 // Paths                                                                      //
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 
-var paths = {
-    src_html    : 'src/index.html',
-    src_js      : 'src/script.js',
-    src_scripts : 'src/**/*.js',
-    src_scss    : 'src/styles/**/*.scss',
+const paths = {
+  src_html   : 'src/index.html',
+  src_js     : 'src/script.js',
+  src_js_all : 'src/**/*.js',
+  src_scss   : 'src/styles/**/*.scss',
 
-    dest        : 'build/',
-    dest_files  : 'build/**/*',
+  dest       : 'build/',
+  dest_files : 'build/**/*',
 };
 
 
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 // Gulp tasks                                                                 //
-////////////////////////////////////////////////////////////////////////////////
+// ========================================================================== //
 
 // Delete generated files
-gulp.task('clean', function (cb) {
-    del.sync(paths.dest, { force: true });
-    cb();
+gulp.task('clean', (cb) => {
+  del.sync(paths.dest, { force: true });
+  cb();
 });
 
 // Process main HTML file
-gulp.task('html', function () {
-    return gulp.src(paths.src_html)
-        .pipe(htmlmin({
-            removeComments: true,
-            collapseWhitespace: true,
-            minifyJS: true
-        }))
-        .pipe(gulp.dest(paths.dest));
+gulp.task('html', () => {
+  return gulp.src(paths.src_html)
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyJS: true
+    }))
+    .pipe(gulp.dest(paths.dest));
 });
 
 // Process SCSS files
-gulp.task('scss', function () {
-    return gulp.src(paths.src_scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(nano())
-        .pipe(gulp.dest(paths.dest));
+gulp.task('scss', () => {
+  return gulp.src(paths.src_scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(nano())
+      .pipe(gulp.dest(paths.dest));
 });
 
 // Process JS files
-gulp.task('js', function () {
-    return browserify(paths.src_js)
-        .transform(babelify, {
-            presets: ['es2015', 'react'],
-            plugins: ['transform-decorators-legacy'],
-        })
-        .bundle()
-        .pipe(source('script.js'))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.dest));
+gulp.task('js', () => {
+  return browserify(paths.src_js)
+    .transform(babelify, {
+      presets: ['es2015', 'react'],
+      plugins: ['transform-decorators-legacy'],
+    })
+    .bundle()
+    .pipe(source('script.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest));
 });
 
 // Default task: compile files
 gulp.task('default', ['clean', 'html', 'scss', 'js']);
 
 // Compile files and recompile on changes
-gulp.task('watch', ['default'], function () {
-    browserSync.init({
-        server: {
-            baseDir: './build'
-        }
-    });
+gulp.task('watch', ['default'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
 
-    gulp.watch(paths.src_html, ['html']);
-    gulp.watch(paths.src_scss, ['scss']);
-    gulp.watch(paths.src_scripts, ['js']);
+  gulp.watch(paths.src_html, ['html']);
+  gulp.watch(paths.src_scss, ['scss']);
+  gulp.watch(paths.src_js_all, ['js']);
 });
 
 // Deploy to GitHub Pages
-gulp.task('deploy', ['default'], function () {
-    return gulp.src(paths.dest_files)
-        .pipe(ghPages());
+gulp.task('deploy', ['default'], () => {
+  return gulp.src(paths.dest_files)
+    .pipe(ghPages());
 });
